e4949aea84c8dd7c975699f8048ca4e64a2773a0
dbland87@gmail.com <#89jw3tWER^#gf&NEpIbl!$Z>
2021-02-07 08:27:54 -0800

    MiCard

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,14 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.only(left: 30.0),
-            padding: EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: Text("Hello"),
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.red,
+                backgroundImage: AssetImage('ic_me.jpg'),
+              ),
+            ],
           ),
         ),
       ),
